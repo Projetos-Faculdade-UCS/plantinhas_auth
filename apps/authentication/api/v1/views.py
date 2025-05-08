a0bbe4d0a25c4da0a3592a622d82b0693bf600ae
Marco Haubrich <marhaubrich@ucs.br>
2025-05-08 17:33:07 -0300

    feat(auth): ✨ Adiciona endpoint de login para administradores
    * Implementa a classe `UserLoginSerializer` para autenticação de usuários administradores.
    * Cria a view `UserLoginView` para gerenciar requisições de login.
    * Adiciona testes para verificar o funcionamento do login de administradores.
    * Atualiza a documentação da API para incluir instruções sobre o novo endpoint de login.

--- apps/authentication/api/v1/views.py
+++ apps/authentication/api/v1/views.py
@@ -4,6 +4,7 @@ from rest_framework.response import Response
 from rest_framework.views import APIView
 
 from .serializers import GoogleAuthSerializer
+from .serializers import UserLoginSerializer
 
 
 class GoogleAuthView(APIView):
@@ -29,4 +30,25 @@ class GoogleAuthView(APIView):
         if serializer.is_valid():
             auth_data = serializer.save()
             return Response(auth_data, status=status.HTTP_200_OK)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+class UserLoginView(APIView):
+    """
+    API View for authenticating admin users with username and password.
+    """
+
+    permission_classes = [AllowAny]
+    authentication_classes = []
+    serializer_class = UserLoginSerializer
+
+    def post(self, request, *args, **kwargs):
+        serializer = UserLoginSerializer(
+            data=request.data, context={"request": request}
+        )
+
+        if serializer.is_valid():
+            auth_data = serializer.save()
+            return Response(auth_data, status=status.HTTP_200_OK)
+
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
